fb4276a0ccbf8c33158da51c4b03c94a776dea36
Luan P. Ramalho <99106030+luanpatrickramalho@users.noreply.github.com>
2022-06-21 13:11:30 -0300

    Aprendendo sobre Ponto Flutuante e Conversão

---
+++ testaConversao.java
@@ -0,0 +1,11 @@
+package cursoDeJavaOrientadoAObjetosAlura;
+
+public class testaConversao {
+
+	public static void main(String[] args) {
+		double salario = 1279.50;
+		int valor = (int)salario;
+		System.out.println(valor);
+	}
+
+}
---
+++ testaPontoFlutuante.java
@@ -0,0 +1,15 @@
+package cursoDeJavaOrientadoAObjetosAlura;
+
+public class testaPontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1250.70;
+		System.out.println("Meu Salario é " +  salario);
+		
+		double divisaoNumeros= 5.0/2;
+		System.out.println("O Resultado da divisao e: " + divisaoNumeros);
+
+	}
+
+}
