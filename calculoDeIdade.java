f455f6f3c9bb450606250ea4ec54bab1a07c6704
Luan P. Ramalho <99106030+luanpatrickramalho@users.noreply.github.com>
2022-06-21 08:28:33 -0300

    Programa que calcula a idade

---
+++ calculoDeIdade.java
@@ -0,0 +1,21 @@
+package cursoDeJavaOrientadoAObjetosAlura;
+import java.util.Scanner;
+public class calculoDeIdade {
+
+	public static void main(String[] args) {
+		int idade;
+		int anoNascimento;
+		int anoAtual;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Informe qual ano voce nasceu:");
+		anoNascimento = teclado.nextInt();
+		System.out.println("Informe qual ano estamos:");
+		anoAtual = teclado.nextInt();
+		
+		idade = anoAtual - anoNascimento;
+		System.out.println("Voce tem " + idade + " Anos.");
+		
+	}
+
+}
